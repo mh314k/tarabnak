cff6a7844ef257051f0dcc9f1d037c1a7f6dab1e
do0k <do0k@outlook.com>
2018-08-26 15:41:05 +0430

    safhe asli & style ha farakhani shod

--- tarabnak/settings.py
+++ tarabnak/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/home/maxx/Dynamic/trbnk/tarabnak/static/',
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 MEDIA_URL = '/media/'
