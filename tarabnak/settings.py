f07a11d70db30dc2ce436bbcf2af73f5fd94b9e0
do0k <do0k@outlook.com>
2018-08-26 15:50:30 +0430

    second commit

--- tarabnak/settings.py
+++ tarabnak/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/home/maxx/Dynamic/trbnk/tarabnak/static/',
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
